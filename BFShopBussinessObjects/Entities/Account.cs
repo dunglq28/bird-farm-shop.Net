1ee83ea7d94f4b6959a3751c95c4f5498e7c0ce4
dunglq28 <lequangdung0803@gamil.com>
2023-11-04 17:14:00 +0700

    update sql in order detail

--- BFShopBussinessObjects/Entities/Account.cs
+++ BFShopBussinessObjects/Entities/Account.cs
@@ -10,14 +10,17 @@ namespace BFShopBussinessObjects.Entities
             Orders = new HashSet<Order>();
         }
 
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Fullname { get; set; }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Fullname { get; set; } = null!;
         public string? Street { get; set; }
         public string? City { get; set; }
+        public string? Phone { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? Gender { get; set; }
         public int? RoleId { get; set; }
 
-        public virtual Role Role { get; set; }
+        public virtual Role? Role { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
     }
 }
